251e804655a075fef80633dc233a0db6bcffbcd8
Dyb3438 <503553364@qq.com>
2019-03-29 19:20:51 +0800

    modify mock api

--- app/Http/Controllers/Mock/MockController.php
+++ app/Http/Controllers/Mock/MockController.php
@@ -9,6 +9,7 @@ class MockController extends Controller
 {
 	public function mock(request $r){
 		$r->session()->put('userid',1);
+		$r->session()->put('user_member',1);
 		$info = User::UserInfoById(1);
 		return response()->json($info);
 	}
